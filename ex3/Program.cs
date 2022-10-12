af787cde47664cbe9d7b82e4ac47a1c03e23c673
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-12 22:42:57 +0300

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти максимальное из трех чисел
+int numberA = new Random() .Next(1, 100);
+Console. WriteLine(numberA);
+int numberB = new Random() .Next(1, 100); 
+Console.WriteLine(numberB);
+int numberC = new Random() .Next(1, 100);
+Console. WriteLine(numberC);
+if (numberA> numberB) 
+if (numberA>numberC)
+{
+    Console.WriteLine(numberA);
+}
+if (numberB> numberA) 
+if (numberB>numberC)
+{
+    Console.WriteLine(numberB);
+}if (numberC> numberB) 
+if (numberC>numberA)
+{
+    Console.WriteLine(numberC);
+}
