0a292b0cb7b3bed022018750ef08ffd1c5db8382
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-12 22:00:24 +0300

    added ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+Console.WriteLine ("number A");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine ("number B");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA*numberA == numberB)
+{
+Console.WriteLine("yes");
+}
+else 
+Console.WriteLine ("no");
