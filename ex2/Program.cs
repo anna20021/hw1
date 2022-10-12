798330edbfac65825af4ff9027b7e89a5995ed00
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-12 22:20:38 +0300

    ex2

---
+++ ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿// По заданному номеру дня недели вывести его название
+Console.WriteLine ("enter number from 1 till 7");
+int number = int.Parse(Console.ReadLine());
+if (number == 1)
+{
+    Console.WriteLine("monday");
+}
+if (number == 2)
+{
+    Console.WriteLine("tuesday");
+}
+if (number == 3)
+{
+    Console.WriteLine("wendsday");
+}
+if (number == 4)
+{
+    Console.WriteLine("thursday");
+}
+if (number == 5)
+{
+    Console.WriteLine("friday");
+}
+if (number == 6)
+{
+    Console.WriteLine("saturday");
+}
+if (number == 7)
+{
+    Console.WriteLine("sunday");
+}
+else 
+{
+    Console.WriteLine("wrong number");
+}
