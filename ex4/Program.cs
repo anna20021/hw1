82f7c10109a4a3f7d81e962bbbeb5be4742a789f
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-12 22:49:18 +0300

    ex4

---
+++ ex4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выяснить является ли число чётным
+Console.WriteLine("enter number");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("четное");
+}
+else 
+{
+    Console.WriteLine("нечетное"); 
+}
