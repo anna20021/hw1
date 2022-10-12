b0725ae26e9042bb0906abe2ec8f6fd3b6f3aac1
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-12 22:52:38 +0300

    ex4

---
+++ ex5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать числа от -N до N
+Console.WriteLine("enter the number");
+int number = int.Parse(Console.ReadLine());
+int number1 = (number) * (-1);
+
+while (number1<=number)
+{
+    int x = number1++;
+    Console.WriteLine(x); 
+}
