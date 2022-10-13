56e55fe214659e9bf36f98e395d0255b17783f2f
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-13 17:23:50 +0300

    ex12

---
+++ ex12/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+int number = new Random() .Next(100, 1000);
+Console. WriteLine(number);
+int a = 0;
+a = number % 10;
+
+if (a == 0)
+{
+    Console. WriteLine("нет остатка");
+}
+else if (a > 0)
+{
+    Console. WriteLine(a);
+}
