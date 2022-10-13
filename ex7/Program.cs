b2420846d593061a87c63d38569b78985d86c65b
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-13 16:11:08 +0300

    ex7

---
+++ ex7/Program.cs
@@ -0,0 +1,6 @@
+﻿//Показать последнюю цифру трёхзначного числа
+Console.WriteLine("enter the number");
+int number = int.Parse (Console.ReadLine());
+int a = 0;
+a = number % 10;
+Console.WriteLine(a);
