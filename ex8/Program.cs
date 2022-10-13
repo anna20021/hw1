69927d57a8df4b0c95a859f489af2d68454b0277
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-13 16:14:10 +0300

    ex8

---
+++ ex8/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+Console.WriteLine("enter the number");
+int number = int.Parse (Console.ReadLine());
+int a = 0;
+a = number / 10; 
+a= a % 10;
+Console.WriteLine(a);
+
