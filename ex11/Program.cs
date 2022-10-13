c131425d6cae7c5823de32c595254d127bc181f9
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-13 17:13:45 +0300

    ex11

---
+++ ex11/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+int numberA = new Random() .Next(10,1000);
+Console. WriteLine(numberA);
+Console.WriteLine("numberB");
+int numberB = int.Parse (Console.ReadLine());
+int a = 0;
+int b = 0;
+
+if (numberA % numberB == 0)
+{
+    a= (numberA % numberB);
+   Console. WriteLine("a"); 
+}
+else if (numberA % numberB != 0)
+{
+    b= numberA % numberB;
+Console. WriteLine(b);
+}
