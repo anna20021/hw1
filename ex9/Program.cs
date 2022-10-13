0f954683031e6ef64b77c53d455d110d44039f50
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-13 16:18:50 +0300

    ex9

---
+++ ex9/Program.cs
@@ -0,0 +1,16 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+int number = new Random() .Next(10, 99);
+Console. WriteLine(number);
+int a = 0;
+int b = 0; 
+a = number / 10; 
+a = a % 10;
+b = number % 10;
+if (a > b)
+{
+    Console. WriteLine(a);
+}
+if (a < b)
+{
+    Console. WriteLine(b);
+}
