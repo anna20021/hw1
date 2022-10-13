2e123b8732aef4c3b1fcbc9bd6a84057982a243b
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-13 13:21:55 +0300

    ex6

---
+++ ex6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать четные числа от 1 до N
+Console.WriteLine ("enter the number");
+int number = int.Parse (Console.ReadLine());
+int x = 1; 
+while (x<=number)
+{
+    
+if (x % 2 == 0)
+{
+Console.WriteLine(x);
+}
+x = x+ 1;
+}
