6e2428e593c64e0d2b39d0301ff0caee1f7fa306
Snezhana <pavalaki.snezhana@mail.ru>
2022-10-13 16:32:57 +0300

    ex10

---
+++ ex10/Program.cs
@@ -0,0 +1,13 @@
+﻿//Удалить вторую цифру трёхзначного числа
+int number = new Random() .Next(100, 1000);
+Console. WriteLine(number);
+int a = 0;
+int b = 0;
+int c = 0;
+int d = 0; 
+a = number / 100; 
+b = number / 10;
+b = b % 10;
+c = number % 10;
+d = a* 10 + c ;
+Console. WriteLine(d);
